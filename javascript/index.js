20d26db64d991a5441dd1bca466e9489a1a10913
freddie <freddie@localhost.localdomain>
2023-09-15 12:04:45 +0300

    provider demo, delete asap

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+/*
+    HANDLE CLICK TO TOGGLE HUMMBUGER MENU
+*/
+let menuBtn = document.getElementById("btn-menu")
+menuBtn.addEventListener("click", ()=>{
+    return 
+})
+
+/*
+    HANDLE CLICK TO HANDLE USER INPUT -> CIPHER INPUT
+*/
+const cipher = require("./ceasarCipher")
+let nameNode = document.getElementById("name");
+let messageNode = document.getElementById("message")
+
+console.log(messageNode)
+
